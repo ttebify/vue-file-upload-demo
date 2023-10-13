b712a9710f8d49e511dcbbc5964830b5be7f77eb
Justice Ekemezie <ttebify@gmail.com>
2023-10-13 11:58:47 +0100

    Use imagekit-vue3 for Vue3 support

--- src/views/ImageKitExample.vue
+++ src/views/ImageKitExample.vue
@@ -9,18 +9,25 @@
   >
     <button>Upload</button>
   </i-k-upload>
+  <!-- Display the uploaded file -->
+  <img v-if="imageUrl" :src="imageUrl" alt="Uploaded file" />
 </template>
 
 <script>
-import { IKUpload } from "imagekitio-vue";
+import { IKUpload, IKCore } from "imagekit-vue3";
+
+const publicKey = process.env.VUE_APP_IMAGEKIT_PUBLIC_KEY;
+const urlEndpoint = process.env.VUE_APP_IMAGEKIT_URL_ENDPOINT;
+const authenticationEndpoint = process.env.VUE_APP_AUTHENTICATION_ENDPOINT;
 
 export default {
   name: "ImageKitExample",
   data() {
     return {
-      publicKey: process.env.VUE_APP_IMAGEKIT_PUBLIC_KEY,
-      urlEndpoint: process.env.VUE_APP_IMAGEKIT_URL_ENDPOINT,
-      authenticationEndpoint: process.env.VUE_APP_AUTHENTICATION_ENDPOINT,
+      publicKey,
+      urlEndpoint,
+      authenticationEndpoint,
+      imageUrl: "",
     };
   },
   components: {
@@ -29,6 +36,18 @@ export default {
   methods: {
     handleSuccess(result) {
       console.log(result);
+      // Generate optimized and transformed URLs for the uploaded files
+      const imagekit = new IKCore({
+        urlEndpoint: this.urlEndpoint,
+        publicKey: this.publicKey,
+      });
+
+      this.imageUrl = imagekit.url({
+        path: result.filePath,
+        transformation: [{ height: 300, width: 400 }],
+      });
+
+      console.log(this.imageUrl);
     },
     handleError(err) {
       console.error(err);
